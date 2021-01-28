539e3c9515fb0f4834b1bdf03400d054a99d3306
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-28 11:35:38 +0800

    Deploy GeekChenYi/GeekChenYi.github.io to github.com/GeekChenYi/GeekChenYi.github.io.git:main

---
+++ assets/js/12.a773c4c5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{507:function(t,s,e){"use strict";e.r(s);var n=e(6),r=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("p",[this._v("这里是文章详情,测试Travis CI的自动化部署任务")])])}),[],!1,null,null,null);s.default=r.exports}}]);
