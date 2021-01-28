35f236d57317bc259dd431d6941549a97baae8e4
wangleishan <GeekChenYi@163.com>
2021-01-28 15:44:21 +0800

    feat:增加服务器部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env sh
 
+# 常量定义
+TIMESPAN=`date +%s`
+TIMEFMT=`date +%Y-%m-%d\ %H:%M:%S`
+DEPLOYNAME=GeekChenYi.docs.${TIMESPAN}
+DEPLOYFILES=${DEPLOYNAME}.tar.gz
+SERVER=39.106.144.159
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
+tar -zcvf ${DEPLOYFILES} ./*
 
-# deploy to github
-echo 'b.xugaoyi.com' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+# deploy to aliyun
+ssh -p 22 -o StrictHostKeyChecking=no root@${SERVER} << prod_deploy
+cd /www/web/site/blog/dist
+rm -rdf ./*
+mkdir tarfiles
+exit;
+prod_deploy
+# 本地文件上传到服务器
+scp -P 22 -o StrictHostKeyChecking=no ${DEPLOYFILES} root@${SERVER}:/www/web/site/blog/dist/tarfiles
+# 压缩包解压
+ssh -p 22 -o StrictHostKeyChecking=no root@${SERVER} tar -xzf /www/web/site/blog/dist/tarfiles/${DEPLOYFILES} -C /www/web/site/blog/dist
+# $?上一条命令的退出码
+if [ $? -ne 0 ]; then
+    echo "fail"
 else
-  msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
+    echo "【提示】执行成功  ${TIMEFMT} "
+    echo "【提示】${DEPLOYFILES} 已成功部署服务器 "
 fi
-git init
-git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
-
-# deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:xgy/xgy.git
-else
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
-
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
