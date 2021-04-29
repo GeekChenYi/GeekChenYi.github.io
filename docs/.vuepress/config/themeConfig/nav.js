60d53e98ec55c694094054ce08b1ba4701dacd2a
wangls1 <wangls1@sit.com.cn>
2021-04-29 17:47:54 +0800

    feat(ts): 增加TS相关文章
    增加TS相关文章

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -3,7 +3,7 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: '前端',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/web/', // 目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
@@ -21,7 +21,7 @@ module.exports = [
           { text: '《ES6 教程》笔记', link: '/note/es6/' },
           { text: '《Vue》笔记', link: '/note/vue/' },
           {
-            text: '《TypeScript 从零实现 axios》',
+            text: '《TypeScript》学习笔记',
             link: '/note/typescript-axios/',
           },
           {
@@ -42,6 +42,7 @@ module.exports = [
     items: [
       { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
       { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+      { text: 'TailWind CSS', link: '/pages/lvdrk6KK2i9w3LlD' },
     ],
   },
   {
